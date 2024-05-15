7b1ed4736f5d8033515648d87a2002330a0f4540
J0113boy <keskitalo_6@hotmail.com>
2024-05-15 08:46:17 +0200

    simple server setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const database = require('./database')
+
+const app = express()
+const PORT = 5000;
+const URL = '127.0.0.1'
+
+app.use(bodyParser.json())
+
+// const db = database.initDatabase() // denna fixar vi på database.js sen 
+
+// frågan är om funktionern (dvs createSubscription, createUser, etc) ska följa här nedan eller om vi ska separera dem i en egen js-fil
+
+
+
+app.listen(PORT, URL, () => {
+    console.log(`Server running at http://${URL}:${PORT}`)
+})
