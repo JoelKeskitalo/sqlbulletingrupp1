3bcc0b957453ccae2702c6cf1baf81a6ad972c59
J0113boy <keskitalo_6@hotmail.com>
2024-05-16 11:26:38 +0200

    tables more or less complete?

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const sqlite = require('sqlite3').verbose() // verbosa förtydligar error-meddelanden
 
 function initDatabase() { // funktionen som skapar själva databasen
-    const db = new sqlite.Database('./sqlBulletinDB', (error) => { // databasen skapas i samma nivå som database.js, om den inte finns, finns den så körs den bara 
+    const db = new sqlite.Database('./sqlBulletinDB.db', (error) => { // databasen skapas i samma nivå som database.js, om den inte finns, finns den så körs den bara 
         if (error) {
             return console.log(error)
         } else {
@@ -14,21 +14,33 @@ function initDatabase() { // funktionen som skapar själva databasen
     let sql_user = `CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
-        email TEXT NOT NULL UNIQUE,
-    )`
+        email TEXT NOT NULL UNIQUE
+    )`;
 
     let sql_channel = `CREATE TABLE IF NOT EXISTS channels (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
         owner_id INTEGER NOT NULL,
         FOREIGN KEY (owner_id) REFERENCES users(id)
-    )`
+    )`;
 
     let sql_message = `CREATE TABLE IF NOT EXISTS messages (
-        
-    )`
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT VARCHAR(200) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        user_id INTEGER NOT NULL,
+        channel_id INTEGER NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        FOREIGN KEY (channel_id) REFERENCES channels(id)
+    )`;
 
-    let sql_subscription = ``
+    let sql_subscription = `CREATE TABLE IF NOT EXISTS subscriptions (
+        user_id INTEGER NOT NULL,
+        channel_id INTEGER NOT NULL,
+        PRIMARY KEY (user_id, channel_id),
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        FOREIGN KEY (channel_id) REFERENCES channels(id)
+    )`;
 
     // db.run ser till att de "körs", dvs skapas, i den ordningen vi har ställt upp dem (serialize)
     db.serialize(() => {
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const URL = '127.0.0.1'
 
 app.use(bodyParser.json()) // ser till att omvandla JSON-format så att vi kan utläsa det 
 
-// const db = database.initDatabase() // denna fixar vi på database.js sen 
+database.initDatabase() 
 
 // frågan är om funktionern (dvs createSubscription, createUser, etc) ska följa här nedan eller om vi ska separera dem i en egen js-fil
 
