e7e194eafdc161ee116b2a4a2e8877105cd95506
GitMicaFri <micaela.fristedt@gmail.com>
2024-05-17 09:57:00 +0200

    Added function createUser

--- functions.js
+++ functions.js
@@ -1,3 +1,34 @@
 // här skapar vi alla våra funktioner, som vi sedan exporterar och slutligen importerar till vår run-fil, dvs index. 
+const database = require (`./database`)
+const db = database.initDatabase()
 
 // funktioner som behövs:
+
+// createUser
+const createUser = async (name, email) => {
+    return new Promise((resolve, reject) => {
+        const sql = `INSERT INTO users (name, email) VALUES(?, ?)`
+        db.run(sql, [name, email], function (error) { // ? är för att undvika SQL injection.
+            if (error) {
+                console.error(error);
+                reject(error);
+             } else {
+                const userId = this.lastID;
+                resolve({userId, name, email})
+            }
+        }); 
+    });
+};
+
+
+
+module.exports = { 
+    createUser
+};
+// postUser
+
+// getUser
+
+// getAllUsers
+
+// deleteUser
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const database = require('./database')
+const { createUser } = require('./functions');
 
 const app = express()
 const PORT = 5000;
@@ -8,10 +9,20 @@ const URL = '127.0.0.1'
 
 app.use(bodyParser.json()) // ser till att omvandla JSON-format så att vi kan utläsa det 
 
-database.initDatabase() 
-
 // frågan är om funktionerna (dvs createSubscription, createUser, etc) ska följa här nedan eller om vi ska separera dem i en egen js-fil
 
+app.post('/users', async (req, res) => {
+    const { name, email } = req.body;
+    if (!name || !email) {
+        return res.status(400).json({ error: 'Name and email are required' });
+    }
+    try {
+        const user = await createUser(name, email)
+        res.status(200).send(user)
+    } catch(error){
+        res.status(500).send(error.message)
+    }
+});
 
 
 app.listen(PORT, URL, () => {
